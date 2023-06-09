e4dc081b73e4aaf8011bafc935921914ce91398d
Rene Rivas <rrivas@teravisiontech.com>
2023-06-08 21:49:16 -0400

    Hello World in Node.js

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log('Hello World');
+
+/*
+ * In order to execute this in Node.js, you have to open a console, navigate to the file directory
+ * and introduce this command in the console: "node <file_name>"; in this case is "node app.js"
+*/
